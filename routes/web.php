68405a267ff0ba26089dd0cc978cefb1034e2d0d
RemMayrina <remmayrina@gmail.com>
2024-07-31 10:20:20 +0800

    Validation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\UsertaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('user_task', UsertaskController::class);
